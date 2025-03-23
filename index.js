f7c977e1d15936385200fafc2a9e37bd9cb9e63e
bharth360 <51966138+bharth360@users.noreply.github.com>
2025-03-23 19:08:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// Health check route for AWS ELB
+app.get("/health", (req, res) => {
+  res.status(200).send("Healthy");  // Respond with HTTP 200 and message "Healthy"
+});
+
+// Your other routes
 app.get("/", (req, res) => {
   res.send("<h1>Hey guys, Welcome to my new Express Application!!</h1>");
 });
@@ -10,6 +16,7 @@ app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
 
-app.listen(3000, () => {
-  console.log("listening");
+// Make sure your app listens on the correct port and on all network interfaces
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server is running on port 4000");
 });
